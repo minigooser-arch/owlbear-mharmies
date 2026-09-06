bc32278473cf37be6be741b716b73bc553abdab9
minigooser-arch <minigooser@gmail.com>
2026-09-06 20:36:51 +0300

    test: restore sea terrain in existing v6 scenes

---
+++ src/storage/seaTerrainMigration.test.ts
@@ -0,0 +1,47 @@
+import { expect, it } from "vitest";
+import { DEFAULT_SETTINGS, DEFAULT_TERRAIN, DEFAULT_TURN_STATE } from "../shared/constants";
+import { migrateSceneState } from "./migrations";
+
+it("restores built-in sea terrain when an existing v6 scene does not contain it", () => {
+  const terrain = structuredClone(DEFAULT_TERRAIN);
+  delete terrain.types.sea;
+
+  const result = migrateSceneState({
+    version: 6,
+    revision: 7,
+    settings: DEFAULT_SETTINGS,
+    sides: [],
+    states: [],
+    relations: {},
+    battleGroups: [],
+    terrain,
+    gridMap: { version: 1, cells: {}, revision: 0 },
+    wars: [],
+    turn: DEFAULT_TURN_STATE,
+    ships: {},
+    navalBattleRequests: [],
+    transportEmbarkRequests: [],
+    activeNavalBattle: null,
+    navalBattleHistory: [],
+    navalRevealUntilTurn: {}
+  });
+
+  expect(result).toMatchObject({
+    ok: true,
+    value: {
+      version: 6,
+      revision: 7,
+      terrain: {
+        types: {
+          sea: {
+            id: "sea",
+            name: "Море",
+            movementDomains: ["SEA"],
+            blocksNavalLos: false,
+            enabled: true
+          }
+        }
+      }
+    }
+  });
+});
