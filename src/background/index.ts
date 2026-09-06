174d67e738c5e184df0fc1065f2c29b686fe572e
minigooser-arch <minigooser@gmail.com>
2026-09-06 14:40:27 +0300

    feat: register interception context menu in persistent background

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,27 @@
 import OBR from "@owlbear-rodeo/sdk";
+import { registerNavalInterceptionContextMenu } from "../owlbear/navalInterceptionContextMenu";
 import { startBackgroundApplication } from "./application";
 
 OBR.onReady(() => {
-  void startBackgroundApplication().then((application) => {
-    window.addEventListener("beforeunload", () => void application.stop(), { once: true });
+  void startBackgroundApplication().then(async (application) => {
+    try {
+      const removeInterceptionContextMenu = await registerNavalInterceptionContextMenu(
+        {
+          create: (entry) => OBR.contextMenu.create(entry),
+          remove: (id) => OBR.contextMenu.remove(id)
+        },
+        application,
+        `${import.meta.env.BASE_URL}icon-1.2.png`
+      );
+      window.addEventListener("beforeunload", () => {
+        void removeInterceptionContextMenu()
+          .catch(() => undefined)
+          .finally(() => application.stop());
+      }, { once: true });
+    } catch (error) {
+      await application.stop().catch(() => undefined);
+      throw error;
+    }
   }).catch((error: unknown) => {
     console.error("Letopis Armies background startup failed", error);
     void OBR.notification.show(
