266294750781b79d3b9f31153c5c881187e5bee9
minigooser-arch <minigooser@gmail.com>
2026-09-14 17:29:18 +0300

    feat: register peace transfer overlay metadata

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -65,6 +65,7 @@ export const METADATA_KEYS = {
   interceptionOverlay: `${EXTENSION_ID}/interception-overlay`,
   mapBrushPreview: `${EXTENSION_ID}/map-brush-preview`,
   navalBattleAreaPreview: `${EXTENSION_ID}/naval-battle-area-preview`,
+  peaceTransferOverlay: `${EXTENSION_ID}/peace-transfer-overlay`,
   peaceTransferOverlay: `${EXTENSION_ID}/peace-transfer-overlay`
 } as const;
 
